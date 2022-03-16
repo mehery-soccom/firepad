cf2c2e0aea0a15ececdb4fd7b0b5d7a3b80e63a2
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-03-16 12:34:04 +0530

    firepad

---
+++ examples/js/fire.config.js
@@ -0,0 +1,12 @@
+function fireConfig(){
+      return {
+          apiKey: "AIzaSyAVpX7gTHZxCKcnk1INxRzkg0j5vvkVRpk",
+          authDomain: "meheryutils.firebaseapp.com",
+          databaseURL: "https://meheryutils-default-rtdb.firebaseio.com",
+          projectId: "meheryutils",
+          storageBucket: "meheryutils.appspot.com",
+          messagingSenderId: "58075259853",
+          appId: "1:58075259853:web:b381f70f316dbbd827da84",
+          measurementId: "G-4N1H0G8XQG"
+   };
+}
